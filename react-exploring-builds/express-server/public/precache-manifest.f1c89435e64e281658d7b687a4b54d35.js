1e171be799b620dd9b1bcfac505d8ee18c1f1cfe
Caitlin Conway <coolcac@gmail.com>
2020-08-21 08:40:46 -0500

    react-express-server project added

---
+++ react-exploring-builds/express-server/public/precache-manifest.f1c89435e64e281658d7b687a4b54d35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80a51ba2a799b1c51971d05334125df1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b206395b52d120a6c713",
+    "url": "/static/css/main.0be56012.chunk.css"
+  },
+  {
+    "revision": "b22c9ba60b0d609baba0",
+    "url": "/static/js/2.cf67a1cd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cf67a1cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b206395b52d120a6c713",
+    "url": "/static/js/main.00e89e5c.chunk.js"
+  },
+  {
+    "revision": "256780cce990331d9974",
+    "url": "/static/js/runtime-main.83a27b56.js"
+  },
+  {
+    "revision": "45f7f4d2e733638d130e2db7dc7b6721",
+    "url": "/static/media/react-builds-cat.45f7f4d2.png"
+  }
+]);
